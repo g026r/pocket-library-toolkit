1e29b586d267f2b26a7edb1afb38c52b00b250f9
g026r <g026r@users.noreply.github.com>
2024-10-01 21:31:37 -0400

    Consolidate

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -8,10 +8,20 @@ import (
 	"io"
 	"slices"
 	"strings"
+	"time"
 )
 
 var EntrySort = func(a, b Entry) int {
 	return cmp.Compare(strings.ToLower(a.Name), strings.ToLower(b.Name))
+}
+
+type Thumbnails struct {
+	Modified bool
+	Images   []Image
+}
+type Image struct {
+	Crc32 uint32
+	Image []byte
 }
 
 type Entry struct {
@@ -129,4 +139,54 @@ func extractName(src []byte) (string, error) {
 	}
 
 	return "", errors.New("could not find 0-terminator")
+}
+
+type PlayTime struct {
+	Added  uint32
+	Played uint32
+	System
+}
+
+func (p *PlayTime) ReadFrom(r io.Reader) (int64, error) {
+	var played uint32
+	if err := binary.Read(r, binary.LittleEndian, &p.Added); err != nil {
+		return 0, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &played); err != nil {
+		return 4, err
+	}
+	p.System = FromPlayedTime(played)
+	p.Played = played - p.PlayOffset()
+
+	return 8, nil
+}
+
+func (p *PlayTime) WriteTo(w io.Writer) (int64, error) {
+	var added, played uint32
+	if p.Added != 0 {
+		added = p.Added
+		played = p.Played
+	} else {
+		// Pocket doesn't know about timezones, so we have to manually apply the offset to get the correct-ish time.
+		// Might get kind of funny around DST changeovers, but I can't be bothered with anything fancier.
+		_, offset := time.Now().Zone()
+		added = uint32(time.Now().Add(time.Second * time.Duration(offset)).Unix())
+	}
+
+	if err := binary.Write(w, binary.LittleEndian, added); err != nil {
+		return 0, err
+	}
+	if err := binary.Write(w, binary.LittleEndian, played+p.PlayOffset()); err != nil {
+		return 4, err
+	}
+
+	return 8, nil
+}
+
+func (p *PlayTime) FormatPlayTime() string {
+	s := p.Played % 60
+	m := (p.Played % 3600) / 60
+	h := p.Played / 3600
+
+	return fmt.Sprintf("%dh %dm %ds", h, m, s)
 }
--- pkg/models/models_test.go
+++ pkg/models/models_test.go
@@ -2,8 +2,10 @@ package models
 
 import (
 	"bytes"
+	"encoding/binary"
 	"slices"
 	"testing"
+	"time"
 )
 
 // rawEntry is the binary representation of entry, as copied from a valid list.bin
@@ -106,4 +108,89 @@ func TestExtractName(t *testing.T) {
 			t.Error("Expected error; got nil")
 		}
 	})
+}
+
+var rawPT = []byte{0x7C, 0x65, 0x79, 0x66, 0x24, 0x26, 0x01, 0x08}
+
+var playtime = PlayTime{
+	Added:  0x6679657c,
+	Played: 0x00012624,
+	System: GBA,
+}
+
+func TestPlayTime_ReadFrom(t *testing.T) {
+	b := bytes.Buffer{}
+	b.Write(rawPT)
+
+	sut := PlayTime{}
+	if n, err := sut.ReadFrom(&b); err != nil {
+		t.Fatal(err)
+	} else if n != 8 {
+		t.Fatalf("Expecred 8 bytes read; read %d", n)
+	}
+
+	if sut.Added != playtime.Added {
+		t.Errorf("Excpected %08x, got %08x", playtime.Added, sut.Added)
+	}
+	if sut.Played != playtime.Played {
+		t.Errorf("Excpected %08x, got %08x", playtime.Played, sut.Played)
+	}
+}
+
+func TestPlayTime_WriteTo(t *testing.T) {
+	t.Parallel()
+
+	t.Run("values set", func(t *testing.T) {
+		t.Parallel()
+		w := &bytes.Buffer{}
+		if _, err := playtime.WriteTo(w); err != nil {
+			t.Fatalf("%v", err)
+		}
+		if b := w.Bytes(); slices.Compare(b, rawPT) != 0 {
+			t.Errorf("Expected %v; got %v", cleanEntry, b)
+		}
+	})
+
+	t.Run("values unset", func(t *testing.T) {
+		t.Parallel()
+		w := &bytes.Buffer{}
+		if _, err := (&PlayTime{System: NGPC}).WriteTo(w); err != nil {
+			t.Fatalf("%v", err)
+		}
+		b := w.Bytes()
+		added := binary.LittleEndian.Uint32(b[:4])
+		play := binary.LittleEndian.Uint32(b[4:])
+		_, offset := time.Now().Zone()
+		// Since it's using time.Now() we're going to give it some wiggle room & say that anything within 10 seconds is fine.
+		if n := time.Since(time.Unix(int64(added), 0).Add(-1 * time.Duration(offset) * time.Second)); n > 10*time.Second {
+			t.Errorf("Expected within 10 seconds; got %d", n)
+		}
+		if play != 0x18000000 {
+			t.Errorf("Expected %08x; got %08x", 0x18000000, play)
+		}
+	})
+}
+
+func TestPlayTime_FormatPlayTime(t *testing.T) {
+	t.Parallel()
+
+	p := PlayTime{}
+	if s := p.FormatPlayTime(); s != "0h 0m 0s" {
+		t.Errorf("Expected '0h 0m 0s'; got '%s'", s)
+	}
+
+	p.Played = 55
+	if s := p.FormatPlayTime(); s != "0h 0m 55s" {
+		t.Errorf("Expected '0h 0m 55s'; got '%s'", s)
+	}
+
+	p.Played = 147
+	if s := p.FormatPlayTime(); s != "0h 2m 27s" {
+		t.Errorf("Expected '0h 2m 27s'; got '%s'", s)
+	}
+
+	p.Played = 11004
+	if s := p.FormatPlayTime(); s != "3h 3m 24s" {
+		t.Errorf("Expected '3h 3m 24s'; got '%s'", s)
+	}
 }
