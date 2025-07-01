06596f6c61613d93e4985d6193a54055507744ad
g026r <g026r@users.noreply.github.com>
2025-07-01 14:12:25 -0400

    refactor: a bit of cleanup

--- cmd/playfix/main.go
+++ cmd/playfix/main.go
@@ -17,26 +17,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	root, err := root.OpenRoot(filepath.Dir(ex))
+	r, err := root.OpenRoot(filepath.Dir(ex))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer root.Close()
+	defer r.Close()
 
-	p, err := io.LoadPlaytimes(root.FS())
+	p, err := io.LoadPlaytimes(r.FS())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	complete := false
-	out, err := root.CreateTemp("System/Played Games", "playtimes_*.tmp")
+	out, err := r.CreateTemp("System/Played Games", "playtimes_*.tmp")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer func() {
 		_ = out.Close()
 		if complete { // Overwrite the original with the temp file if successful; delete it if not.
-			err = root.Rename(fmt.Sprintf("System/Played Games/%s", filepath.Base(out.Name())), "System/Played Games/playtimes.bin")
+			err = r.Rename(fmt.Sprintf("System/Played Games/%s", filepath.Base(out.Name())), "System/Played Games/playtimes.bin")
 		} else {
 			err = os.Remove(out.Name())
 		}
